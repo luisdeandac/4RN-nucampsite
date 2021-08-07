b24bd20c7b64998aca54aaa60fdd83f3d68c2604
Luis De Anda <luis.deandac@gmail.com>
2021-08-06 23:54:51 -0500

    Using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,34 +1,30 @@
 import React, { Component } from 'react';
-import { FlatList, View } from 'react-native';
-import { Text, ScrollView  } from 'react-native';
-import { Card, ListItem, } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
+import { ScrollView, Text, FlatList } from 'react-native';
+import { Card, ListItem } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
 
 function Mission({item}) {
-    if (item) {
-        return (
-            <Card title="Our Mission">
-                <Text style={{margin: 10}}>
-                    We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. 
-                    We increase access to adventure for the public while promoting safe and respectful use of resources. 
-                    The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. 
-                    We also present a platform for campers to share reviews on campsites they have visited with each other.
-                </Text>
-            </Card>
-        );
-    }
-    return <View />;
+    return (
+        <Card title="Our Mission">
+            <Text style={{margin: 10}}>
+                We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. 
+                We increase access to adventure for the public while promoting safe and respectful use of resources. 
+                The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. 
+                We also present a platform for campers to share reviews on campsites they have visited with each other.
+            </Text>
+        </Card>
+    );
 }
 
 
 class About extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            partners: PARTNERS
-        };
-    }
 
     static navigationOptions = {
         title: 'About Us'
@@ -40,24 +36,23 @@ class About extends Component {
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             );
         };
         return (
             <ScrollView>
-                <Mission item={this.state.partners.filter(partner => partner.featured)[0]}/>
+                <Mission />
                 <Card title="Community Partners">
-                    <FlatList
-                        data={this.state.partners}
+                    <FlatList 
+                        data={this.props.partners.partners}
                         renderItem={renderPartner}
                         keyExtractor={item => item.id.toString()}
-                    >
-                    </FlatList>
+                    />
                 </Card>
             </ScrollView>
         );
     }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = '192.168.0.11:3001/';
+export const baseUrl = 'http://192.168.0.11:3001/';
